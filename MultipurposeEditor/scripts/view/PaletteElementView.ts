9fa3c43a1921ec1b8723e58b69dc81b4713b2a79
Yuniya Kim <kim.yuniya@mail.ru>
2019-11-25 22:25:32 +0300

    Added PaletteElementView

--- MultipurposeEditor/scripts/view/PaletteElementView.ts
+++ MultipurposeEditor/scripts/view/PaletteElementView.ts
@@ -1,1 +1,15 @@
-﻿
+﻿export class PaletteElementView {
+    readonly content: string;
+    private width: number;
+    private height: number;
+
+    constructor(name: string, elementClass: string, type: string, imageSource: string) {
+        this.content = 
+            `<li>` +
+            `   <div class="${elementClass}" type="${type}">` +
+            `       <img src="${imageSource}" width="${this.width}" height="${this.height}">` +
+            `       ${name}` +
+            `   </div>` +
+            `</li>`;
+    }
+}
