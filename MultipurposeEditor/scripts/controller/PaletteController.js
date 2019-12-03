531fe6ca2173cf49930a39ee47bb5e1e5fe77833
Yuniya Kim <kim.yuniya@mail.ru>
2019-12-03 03:00:53 +0300

    Commented on odd code

--- MultipurposeEditor/scripts/controller/PaletteController.js
+++ MultipurposeEditor/scripts/controller/PaletteController.js
@@ -18,9 +18,9 @@ export class PaletteController {
     AppendPaletteElement() {
         var paletteElement = new PaletteElementView();
         $(".tree-element").append(paletteElement.Content);
-        $(".tree-element").treeview({
-            persist: "location"
-        });
+        //$(".tree-element").treeview({
+        //    persist: "location"
+        //});
     }
     ClearContent() {
         $(".tree-element").empty();
