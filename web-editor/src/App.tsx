3ab41e694242860fd7158ae088ea1f8bce3e161d
Yuniya Kim <kim.yuniya@mail.ru>
2021-03-22 11:48:24 +0300

    Added custom node and get request

--- web-editor/src/App.tsx
+++ web-editor/src/App.tsx
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
-import ReactFlow, {addEdge, Background, Elements, removeElements, Edge, Connection} from 'react-flow-renderer';
-import {initialElements} from './initialElements';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useHotkeys } from 'react-hotkeys-hook';
+import ReactFlow, { addEdge, Background, Elements, removeElements, Edge, Connection } from 'react-flow-renderer';
+
+import { initialElements } from './initialElements';
+import customNode from './customNodes';
 import "./App.css"
 
 const onLoad = (reactFlowInstance: { fitView: () => void; }) => {
     console.log('flow loaded:', reactFlowInstance);
     reactFlowInstance.fitView();
 };
+const nodeTypes = {
+    exampleNode: customNode,
+};
 
 const OverviewFlow = () => {
     const [elements, setElements] = useState(initialElements);
+    useHotkeys('delete', () => console.log("Delete pressed"));
     const onElementsRemove = (elementsToRemove: Elements) : void => {
         setElements((elements: Elements) => removeElements(elementsToRemove, elements));
         console.log('elements:', elements);
@@ -19,6 +27,12 @@ const OverviewFlow = () => {
         console.log('elements:', elements);
     }
 
+    useEffect(() => {
+        axios
+            .get("http://localhost:8004/api/Model/all")
+            .then(response => console.log(response.data));
+    }, []);
+
     return (
         <div className="diagram-container">
             <ReactFlow
@@ -26,6 +40,7 @@ const OverviewFlow = () => {
                 onElementsRemove={onElementsRemove}
                 onLoad={onLoad}
                 onConnect={onConnect}
+                nodeTypes={nodeTypes}
                 deleteKeyCode={46}
                 snapToGrid={true}
                 snapGrid={[25, 25]}
