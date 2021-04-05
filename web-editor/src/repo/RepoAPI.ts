99bf86636ce183f01e24a5331c45cd1a6617205b
Mikhail Kidiankin <mikhail.kidiankin@jetbrains.com>
2021-04-05 20:30:00 +0300

    Add repo api element controller methods

--- web-editor/src/repo/RepoAPI.ts
+++ web-editor/src/repo/RepoAPI.ts
@@ -1,6 +1,12 @@
 import {Requests} from "./Requests";
 import {ModelInfo} from "../model/ModelInfo";
 import {Model} from "../model/Model";
+import {Relationship} from "../model/Relationship";
+import {Association} from "../model/Association";
+import {Node} from "../model/Node"
+import {Generalization} from "../model/Generalization";
+import {Attribute} from "../model/Attribute";
+import {Slot} from "../model/Slot";
 
 export class RepoAPI {
     static host: string = "http://localhost:8000/api/repo"
@@ -17,7 +23,7 @@ export class RepoAPI {
     }
 
     public static DeleteModel(modelName: string) {
-        Requests.Request("DETELE", "${RepoAPI.host}/Model/${modelName}")
+        Requests.Request("DETELE", `${RepoAPI.host}/Model/${modelName}`)
     }
 
     public static CreateDeepMetamodel(modelName: string) {
@@ -32,13 +38,90 @@ export class RepoAPI {
         return Requests.Request("PUT", `${RepoAPI.host}/Model/${oldName}/name/${newName}`)
     }
 
+
+    // Elements
+
+    public static GetElement(modelName: string, elementName: string): Element | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/${modelName}/${elementName}`))
+    }
+
+    public static DeleteElement(modelName: string, elementName: string) {
+        Requests.Request("DELETE", `${RepoAPI.host}/Element/${modelName}/${elementName}`)
+    }
+
+    public static GetNode(modelName: string, elementName: string): Node | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/node/${modelName}/${elementName}`))
+    }
+
+    public static GetRelationship(modelName: string, elementName: string): Relationship | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/relationship/${modelName}/${elementName}`))
+    }
+
+    public static GetAssociation(modelName: string, elementName: string): Association | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/association/${modelName}/${elementName}`))
+    }
+
+    public static CreateNode(modelName: string, name: string, level: number, potency: number): Node | undefined {
+        return this.ParseJson(Requests.Request("POST", `${RepoAPI.host}/Element/node/${modelName}/${name}/${level}/${potency}`))
+    }
+
+    public static CreateGeneralization(modelName: string, sourceName: string, targetName: string, level: number, potency: number): Generalization | undefined {
+        return this.ParseJson(Requests.Request("POST", `${RepoAPI.host}/Element/generalization/${modelName}/${sourceName}/${targetName}/${level}/${potency}`))
+    }
+
+    public static CreateAssociations(modelName: string, name: string, sourceName: string, targetName: string,
+                                     level: number, potency: number,
+                                     minSource: number, maxSource: number, minTarget: number, maxTarget: number): Node | undefined {
+        return this.ParseJson(Requests.Request("POST", `${RepoAPI.host}/Element/association/${modelName}/${name}/${sourceName}/${targetName}/${level}/${potency}/${minSource}/${maxSource}/${minTarget}/${maxTarget}`))
+    }
+
+    public static InstantiateNode(modelName: string, name: string, parentName: string, level: number, potency: number): Node | undefined {
+        return this.ParseJson(Requests.Request("POST", `${RepoAPI.host}/Element/node/${modelName}/${parentName}/${name}/${level}/${potency}`))
+    }
+    
+    public static InstantiateAssociation(modelName: string, parentName: string, sourceName: string, targetName: string,
+                                         level: number, potency: number,
+                                         minSource: number, maxSource: number, minTarget: number, maxTarget: number): Association | undefined {
+        return this.ParseJson(Requests.Request("POST", `${RepoAPI.host}/Element/association/${modelName}/${parentName}/${sourceName}/${targetName}/${level}/${potency}/${minSource}/${maxSource}/${minTarget}/${maxTarget}`))
+    }
+
+    public static GetAttributes(modelName: string, name: string): Attribute[] | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/${modelName}/${name}/attributes`))
+    }
+
+    public static GetAttribute(modelName: string, elementName: string, attributeName: string): Attribute | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/${modelName}/${elementName}/attribute/${attributeName}`))
+    }
+
+    public static AddAttribute(modelName: string, elementName: string, attributeName: string, typeName: string, level: number, potency: number): Attribute | undefined {
+        return this.ParseJson(Requests.Request("POST", `${RepoAPI.host}/Element/${modelName}/${elementName}/attribute/${attributeName}/${typeName}/${level}/${potency}`))
+    }
+
+    public static SetAttributeSingle(modelName: string, elementName: string, attributeName: string, single: boolean): Attribute | undefined {
+        return this.ParseJson(Requests.Request("PUT", `${RepoAPI.host}/Element/${modelName}/${elementName}/attribute/${attributeName}/${single}`))
+    }
+
+    public static GetSlots(modelName: string, name: string): Slot[] | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/${modelName}/${name}/slots`))
+    }
+
+    public static GetSlot(modelName: string, elementName: string, attributeName: string): Slot | undefined {
+        return this.ParseJson(Requests.Request("GET", `${RepoAPI.host}/Element/${modelName}/${elementName}/slot/${attributeName}`))
+    }
+
+    public static AddSlot(modelName: string, elementName: string, attributeName: string, valueName: string, level: number, potency: number): Slot | undefined {
+        return this.ParseJson(Requests.Request("POST", `${RepoAPI.host}/Element/${modelName}/${elementName}/slot/${attributeName}/${valueName}/${level}/${potency}`))
+    }
+
+    public static SetSlotValue(modelName: string, elementName: string, attributeName: string, valueName: string): Slot | undefined {
+        return this.ParseJson(Requests.Request("PUT", `${RepoAPI.host}/Element/${modelName}/${elementName}/slot/${attributeName}/${valueName}`))
+    }
+
     private static ParseJson<Type>(json: string | undefined): Type | undefined {
         if (json === undefined) {
             return undefined;
         }
         return JSON.parse(json)
     }
-    
-
 
 }
