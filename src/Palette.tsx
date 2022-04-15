fe67720b084ad91a408ff223e19cd8565e55929c
Yuniya Kim <kim.yuniya@mail.ru>
2022-04-15 22:41:44 +0300

    Update Palette

--- src/Palette.tsx
+++ src/Palette.tsx
@@ -1,47 +1,54 @@
 import React, {DragEvent, useEffect, useState} from 'react';
 
 import './Palette.css';
-import './RobotsModelNode.css';
-import './Nodes.css'
+import './nodes/QueryNodes.css';
+// import './nodes/RobotsModelNode.css';
+// import './nodes/Nodes.css'
 
-import ImageNodeList from './nodesWithImages/ImageNodeList';
-import {getMetamodel} from './requests/modelRequests';
-import {getAttributeValue} from './requests/attributesRequests';
+// import ImageNodeList from './nodes/ImageNodeList';
+import {getModel} from './requests/modelRequests';
+import {getAttributeValue} from './requests/attributeRequests';
 
-const onDragStart = (event: DragEvent, nodeType: string, elementId: number) => {
-    event.dataTransfer.setData('application/reactflow', `${nodeType} ${elementId}`);
+const onDragStart = (event: DragEvent, kind: string, id: number) => {
+    event.dataTransfer.setData('application/reactflow', `${kind} ${id}`);
     event.dataTransfer.effectAllowed = 'move';
 };
 
 const Palette = (props: { metamodelName: string }) => {
-    const [metamodel, setMetamodel] = useState<Array<{ id: number, name: string }>>([]);
+    const [metamodel, setMetamodel] = useState<Array<{ id: number, name: string, kind: string }>>([]);
 
     useEffect(() => {
-        let newMetamodel: Array<{ id: number, name: string }> = [];
-        getMetamodel(props.metamodelName).then(data => {
+        let newMetamodel: Array<{ id: number, name: string, kind: string }> = [];
+        getModel(props.metamodelName).then(data => {
             if (data !== undefined) {
                 data.forEach((element: { id: number, name: string }) => {
-                    getAttributeValue(props.metamodelName, element.id, 'isAbstract').then(data => {
-                        if (data !== undefined && !data && element.name !== 'Link') {
-                            newMetamodel.push(element);
-                        }
-                    });
+                    if (element.name !== 'Link') {
+                        getAttributeValue(props.metamodelName, element.id, 'isAbstract').then(data => {
+                            if (!data) {
+                                getAttributeValue(props.metamodelName, element.id, 'kind').then(data => {
+                                    if (data !== undefined) {
+                                        newMetamodel.push({id: element.id, name: element.name, kind: data});
+                                    }
+                                });
+                            }
+                        });
+                    }
                 });
             }
         }).finally(() => setMetamodel(newMetamodel));
     }, []);
 
-    const RobotsNodePaletteItem = (props: { element: { id: number; name: string } }) => {
+    const PaletteItem = (props: { element: { id: number; name: string, kind: string } }) => {
         return (
-            <div className="robotsNode" key={props.element.id}
-                 onDragStart={(event: DragEvent) => onDragStart(event, 'robotsNode', props.element.id)} draggable>
-                {props.element.name}
+            <div className={`${props.element.kind}Node`} key={props.element.id}
+                 onDragStart={(event: DragEvent) => onDragStart(event, props.element.kind, props.element.id)} draggable>
+                {props.element.kind !== 'materializationPlank' ? props.element.name : ''}
             </div>
         );
     }
 
     const metamodelFiltered = metamodel.filter((element) => element.name !== '');
-    const metamodelElements = metamodelFiltered.map(element => <RobotsNodePaletteItem element={element} key={element.name + element.id} />);
+    const metamodelElements = metamodelFiltered.map(element => <PaletteItem element={element} key={element.name + element.id} />);
 
     return (
         <aside>
