5b2914e41b639a79554e9186be9179f5433a814e
Yuniya Kim <kim.yuniya@mail.ru>
2022-05-03 01:09:06 +0300

    Try to fix release

--- src/requests/api.ts
+++ src/requests/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://query-editor-posdb-frontend.herokuapp.com' // http://localhost:5000'
+    baseURL: 'http://localhost:5000' //'https://query-editor-posdb-frontend.herokuapp.com' //
 });
